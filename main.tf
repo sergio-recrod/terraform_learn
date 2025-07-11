f6e195307743a3b49ed1656b38a6647091d4daec
Sergio Recio <sergio.recrod@gmail.com>
2025-07-11 16:17:56 +0100

    initial commit

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+	region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+	ami 			= "ami-05ffe3c48a9991133"
+	instance_type 	= "t3.micro"
+
+	tags = {
+		Name = "terraform-example"
+	}
+}
